dda592c59053a252521a3192a64aed29730d29b0
Stepan Barta <barta.stepi@gmail.com>
2023-10-06 08:48:26 +0200

    zalozeni angular projektu a vytvorena home page s navigation barem(menu)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ClickOutsideDirective } from './navbar/clickOuside.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ClickOutsideDirective
   ],
   imports: [
     BrowserModule,
