dc86260ae27f4d8a942d7ade26ac98cdaa8b36ec
Stepiix <barta.stepi@gmail.com>
2023-11-23 18:01:53 +0100

    guaranted subjects start

--- frontend/src/app/guaranteed-subjects/guaranteed-subjects.component.ts
+++ frontend/src/app/guaranteed-subjects/guaranteed-subjects.component.ts
@@ -1,20 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthorizationService } from '../services/auth.service';
 import { Router } from '@angular/router';
+import { UsersService } from '../services/users.service';
 @Component({
   selector: 'app-guaranteed-subjects',
   templateUrl: './guaranteed-subjects.component.html',
   styleUrls: ['./guaranteed-subjects.component.css']
 })
-export class GuaranteedSubjectsComponent {
-  constructor(private authService: AuthorizationService, private router: Router) {}
+export class GuaranteedSubjectsComponent implements OnInit{
+  subject: {
+    code: string;
+    name: string;
+    annotation: string;
+    guarantor: number;
+  } = {
+    code: '',
+    name: '',
+    annotation: '',
+    guarantor: 0,
+  };
+  subjects: any[] = [];
+
+  constructor(private authService: AuthorizationService, private router: Router, private usersService: UsersService) {}
 
   ngOnInit(): void {
+    this.loadMySubjects();
     if (this.authService.isAuthorized('teacher')) {
       console.log('ScheduleComponent initialized for authorized student.');
     } else {
       this.router.navigate(['/']);
       this.authService.showUnauthorizedAlert();
     }
+  }
+  loadMySubjects() {
+    this.usersService.getMySubjects().subscribe((data: any) => {
+      this.subjects = data.records;
+    });
   }
 }
