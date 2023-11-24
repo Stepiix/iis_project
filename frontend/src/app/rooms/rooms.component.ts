8c2c91cfb13d003a8d148534ce8102d9884299b0
Stepiix <barta.stepi@gmail.com>
2023-11-24 16:23:37 +0100

    fixy na zadani povinnych veci

--- frontend/src/app/rooms/rooms.component.ts
+++ frontend/src/app/rooms/rooms.component.ts
@@ -42,6 +42,11 @@ export class RoomsComponent implements OnInit{
     });
   }
   onSubmit() {
+    if(!this.room.code){
+      this.showCreatedAlert2();
+      return;
+    }
+
     this.isFormVisible = !this.isFormVisible;
     this.addButtonText = "Add Room";
     this.showCreatedAlert();
@@ -110,6 +115,27 @@ export class RoomsComponent implements OnInit{
     welcomeAlert.style.padding = '15px';
     welcomeAlert.style.width = '100%';
     welcomeAlert.style.background = '#00FF00';
+    welcomeAlert.style.color = 'white';
+    welcomeAlert.style.borderRadius = '5px';
+    welcomeAlert.style.whiteSpace = 'nowrap';
+    welcomeAlert.style.textAlign = 'center';
+    document.body.appendChild(welcomeAlert);
+
+    // Automatické skrytí alertu po 2 sekundách (2000 ms)
+    window.setTimeout(() => {
+      welcomeAlert.style.display = 'none';
+    }, 2000);
+  }
+  showCreatedAlert2() {
+    const welcomeAlert = document.createElement('div');
+    welcomeAlert.textContent = 'Je nutné zadat Room code';
+    welcomeAlert.style.position = 'fixed';
+    welcomeAlert.style.top = '10%';
+    welcomeAlert.style.left = '50%';
+    welcomeAlert.style.transform = 'translate(-50%, -50%)';
+    welcomeAlert.style.padding = '15px';
+    welcomeAlert.style.width = '100%';
+    welcomeAlert.style.background = '#FF0000';
     welcomeAlert.style.color = 'white';
     welcomeAlert.style.borderRadius = '5px';
     welcomeAlert.style.whiteSpace = 'nowrap';
